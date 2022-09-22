d6ca57683e553e77ce03ea7ca3e718a71b45910a
Shannon Duvall <sduvall2@elon.edu>
2022-09-22 14:23:51 -0400

    New stuff

--- src/testRepo/Hello.java
+++ src/testRepo/Hello.java
@@ -12,6 +12,7 @@ public class Hello {
 
     public void sayHello (String name) {
         System.out.println("Hello " + name);
+        System.out.println("This is Shannon's new work.  Shouldn't be in main yet.");
     }
 
     /**
