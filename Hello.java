39bebcb817dcb1022dac162371b081db98094fa9
Robert Duvall <rcd@cs.duke.edu>
2022-09-22 11:33:15 -0400

    Hello :)

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/**
+ * Feel free to completely change this code or delete it entirely. 
+ */
+public class Hello {
+    /**
+     * A method to test (and a joke :).
+     */
+    public double getVersion () {
+        return 0.001;
+    }
+
+    /**
+     * Start of the program.
+     */
+    public static void main (String[] args) {
+        System.out.println("Hello world");
+    }
+}
