48054d39064e3fa192bd086b553aa954d6d74ba0
Shannon Duvall <sduvall2@elon.edu>
2022-09-22 14:38:54 -0400

    Update Hello.java

--- src/testRepo/Hello.java
+++ src/testRepo/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
      * A method to test (and a joke :).
      */
     public double getVersion () {
-        return 0.001;
+        return 0.003;
     }
 
     public void sayHello (String name) {
